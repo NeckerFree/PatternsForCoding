73d45c8362ba1bbacaa204d74caebf72f2ef0962
Elio Cortés <elio.cortes3000@gmail.com>
2023-04-21 17:42:19 -0500

    Implemented two pointers pattern

---
+++ Algorithms/Patterns.cs
@@ -0,0 +1,19 @@
+﻿namespace Algorithms
+{
+    public class Patterns
+    {
+        public static int[] TargetSum(int[] input, int target)
+        {
+            int pointer1 = 0;
+            int pointer2 = input.Length - 1;
+            while (pointer1 < pointer2)
+            {
+                int sum = input[pointer1] + input[pointer2];
+                if (sum == target) return new int[] { pointer1, pointer2 };
+                if (sum < target) pointer1++;
+                else pointer2--;
+            }
+            return new int[] { -1, -1 };
+        }
+    }
+}
