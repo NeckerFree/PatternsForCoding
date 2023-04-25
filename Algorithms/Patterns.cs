9e38ffa1587ad9af83d44305d76b67f0ffc71771
Elio Cortés <elio.cortes3000@gmail.com>
2023-04-24 23:16:59 -0500

    Solved Dutch flag challenge

--- Algorithms/Patterns.cs
+++ Algorithms/Patterns.cs
@@ -1,7 +1,54 @@
-﻿namespace Algorithms
+﻿using System.Drawing;
+using System.Security.Cryptography;
+
+namespace Algorithms
 {
     public class Patterns
     {
+    //    procedure three-way-partition(A : array of values, mid : value) :
+    //i ← 0
+    //j ← 0
+    //k ← size of A - 1
+
+    //while j <= k:
+    //    if A[j] < mid:
+    //        swap A[i] and A[j]
+    //        i ← i + 1
+    //        j ← j + 1
+    //    else if A[j] > mid:
+    //        swap A[j] and A[k]
+    //        k ← k - 1
+    //    else:
+    //        j ← j + 1
+
+        public static int[] DutchFlag(int[] input)
+        {
+            //i ≤ j ≤ k.
+            int i = 0;
+            int j = 0;
+            int k = input.Length - 1;
+            while (j <= k)
+            {
+
+                if (input[j] < 1)
+                {
+                    (input[i], input[j]) = (input[j], input[i]);
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    if (input[j] > 1)
+                    {
+                        (input[k], input[j]) = (input[j], input[k]);
+                        k--;
+                    }
+                    else j++;
+                }
+            }
+            return input;
+        }
+
         public static int[] TargetSum(int[] input, int target)
         {
             int pointer1 = 0;
--- TestValidations/UnitTestPatterns.cs
+++ TestValidations/UnitTestPatterns.cs
@@ -5,12 +5,26 @@ namespace TestValidations
     public class UnitTestPatterns
     {
         [Theory]
-        [InlineData(new int[] {1, 2, 3, 4, 6},6, new int[] { 1, 3 } )]
-        [InlineData(new int[] { 2, 5, 9, 11 }, 11, new int[] { 0,2 })]
-        public void ValidateTwoPointers(int[] input,int target, int[] expected)
+        [InlineData(new int[] { 1, 2, 3, 4, 6 }, 6, new int[] { 1, 3 })]
+        [InlineData(new int[] { 2, 5, 9, 11 }, 11, new int[] { 0, 2 })]
+        public void ValidateTwoPointers(int[] input, int target, int[] expected)
         {
-           int[] actual= Patterns.TargetSum(input, target);
+            int[] actual = Patterns.TargetSum(input, target);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 0, 2, 1, 0 }, new int[] { 0, 0, 1, 1, 2 })]
+        [InlineData(new int[] { 2, 2, 0, 1, 2, 0 }, new int[] { 0, 0, 1, 2, 2, 2 })]
+        [InlineData(new int[] { 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2 })]
+        [InlineData(new int[] { 1, 2, 2, 2, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2 })]
+
+
+        public void ValidateDutchFlagChallenge(int[] input, int[] expected)
+        {
+            int[] actual = Patterns.DutchFlag(input);
             Assert.Equal(expected, actual);
         }
     }
+
 }
